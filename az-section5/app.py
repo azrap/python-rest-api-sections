50c991f033f5be79a08c23dd22a94161663b98a7
azrap <azraap@gmail.com>
2020-02-06 17:54:20 -0800

    adding a sqlite3 db - section 5

--- az-section5/app.py
+++ az-section5/app.py
@@ -1,52 +1,50 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
 
+from security import authenticate, identity
 
 app = Flask(__name__)
-
+app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
+app.secret_key = 'jose'
 api = Api(app)
 
-items = []
-# resource can be accessed with a GET method
+jwt = JWT(app, authenticate, identity)
 
+items = []
 
 class Item(Resource):
     parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
 
-    # reqparse lets u specify things you want to go in the body and no other things
-    parser.add_argument('price', type=float, required=True,
-                        help="This field cannot be left blank!")
-
+    @jwt_required()
     def get(self, name):
-        # search items, return x if x['name'] is name, or return None
-        # next gets the first item (or None if returns None)
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': None}, 200 if item is not None else 404
+        return {'item': next(filter(lambda x: x['name'] == name, items), None)}
 
     def post(self, name):
-
-        # if the item with that name already exists in items
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {"message": f'an item with name {name} already exists'}, 400
+            return {'message': "An item with name '{}' already exists.".format(name)}
 
-        # else create the item:
         data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
-        return item, 201
+        return item
 
+    @jwt_required()
     def delete(self, name):
-        # specifying that we want to use the global items variable
         global items
-        # filter x
         items = list(filter(lambda x: x['name'] != name, items))
-        return {"message": "Item deleted"}
+        return {'message': 'Item deleted'}
 
+    @jwt_required()
     def put(self, name):
-
-        # parses the arguments and puts the valid ones in data (eg price in this case)
         data = Item.parser.parse_args()
+        # Once again, print something not in the args to verify everything works
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
@@ -55,18 +53,12 @@ class Item(Resource):
             item.update(data)
         return item
 
-
 class ItemList(Resource):
     def get(self):
         return {'items': items}
 
-
-# this is how you do it without the decorater
-# localhost:5000/item/chair
 api.add_resource(Item, '/item/<string:name>')
-
-# localhost:5000/items
 api.add_resource(ItemList, '/items')
 
-# if missing, defaults to 5000
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)  # important to mention debug=True
